92f15223513cdec1123551a54a384196a1e63327
skodowski <m.t.skodowski@gmail.com>
2018-03-14 21:19:59 +0100

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'STREAM',
+  envName: 'PROD',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAmW1FHU3IR_XPIVnHWtymJX7jHobyd7_k',
+    authDomain: 'cloudtest-40a2c.firebaseapp.com',
+    databaseURL: 'https://cloudtest-40a2c.firebaseio.com',
+    projectId: 'cloudtest-40a2c',
+    storageBucket: 'cloudtest-40a2c.appspot.com',
+    messagingSenderId: '775543450109'
+  }
 };
