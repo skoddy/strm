1ce5dec908c0a7c29997455376fef0fbe4fc0901
Michael Skodowski <m.skodowski@tnnk.local>
2018-03-15 12:16:47 +0100

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { routerTransition } from '@app/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [routerTransition],
-  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'app';
@@ -14,8 +14,15 @@ export class AppComponent {
     { icon: 'home', name: 'Home', aria: 'Home', url: '/' },
     { icon: 'home', name: 'Posts', aria: 'Posts', url: '/asd' }
   ];
-  constructor() {
 
+  options: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      'fixed': false,
+      'top': 0,
+      'bottom': 0,
+    });
   }
 
 }
